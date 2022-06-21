a353e77ff1095c7645d77cd4c5f7e05d42c902fa
abhinesh-kourav <abhineshkourav@gmail.com>
2022-06-21 13:44:43 +0530

    Exception handling

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    try:
+        raise Exception('We are testing custom exception.')
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error)
+
+    logging.info('We are testing logging module')
     return 'Learned how to create a CI CD pipeline!!!'
 
 if __name__ == '__main__':
