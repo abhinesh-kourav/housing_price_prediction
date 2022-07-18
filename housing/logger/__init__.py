782295efc89f7d4a3ae8d98c49f310c7c5d09ce5
abhinesh-kourav <83655052+abhinesh-kourav@users.noreply.github.com>
2022-07-18 14:47:54 +0530

    logger updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -2,22 +2,25 @@ import logging
 from datetime import datetime
 import os
 import pandas as pd
+from housing.constant import get_current_time_stamp 
+LOG_DIR="logs"
 
-LOG_DIR = 'housing_logs'
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
 
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+LOG_FILE_NAME=get_log_file_name()
 
-LOG_FILE_NAME = F"log_{CURRENT_TIME_STAMP}.log"
+os.makedirs(LOG_DIR,exist_ok=True)
 
-os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
 
 logging.basicConfig(filename=LOG_FILE_PATH,
-                    filemode='w',
-                    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO
-                    )
+filemode="w",
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
+)
 
 def get_log_dataframe(file_path):
     data=[]
